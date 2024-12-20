6ed16e986965558a2577826be994c50ad906a070
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2024-12-20 21:03:08 +0100

    Ingredients  Edit form finished
    I need to save the update to reflect the chage on the global recipes list.

--- frontFoodieLife/src/app/components/recipe/recipe-detail/recipe-detail.component.ts
+++ frontFoodieLife/src/app/components/recipe/recipe-detail/recipe-detail.component.ts
@@ -8,11 +8,13 @@ import { ActivatedRoute } from '@angular/router';
 import { NgIf, NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ReturnStatement } from '@angular/compiler';
+import { Step } from '../Step';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-detail',
   standalone: true,
-  imports: [NgFor, NgIf, RouterLink],
+  imports: [NgFor, NgIf, RouterLink, FormsModule],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
@@ -29,10 +31,19 @@ export class RecipeDetailComponent {
     steps: [],
     ingredientQuantity: []
   };
-  edit = false;
   ingredients: Ingredient[] = [];
+  recipeToEdit: Recipe = {
+    id: 0,
+    name: '',
+    ingredients: [],
+    steps: [],
+    ingredientQuantity: []
+  }; 
+  edit = false;
+
   ngOnInit() {
     this.getRecipe();
+    this.recipeToEdit = this.recipe;
   }
   getRecipe() {
     let id = Number(this.route.snapshot.paramMap.get('id'))!;
@@ -55,12 +66,55 @@ export class RecipeDetailComponent {
       console.log("Ingredient fetched: " + JSON.stringify(this.ingredients[id]));
     }
   }
-  getRecipeIngredientQuantity(id : number) : number{
+  getRecipeIngredientQuantity(id: number): number {
     let quantity = 0;
-    quantity = this.recipe.ingredientQuantity.find( i => i.idIngredient == id)!.quantity;
+    quantity = this.recipe.ingredientQuantity.find(i => i.idIngredient == id)!.quantity;
     return quantity;
   }
-  changeEdit(){
+  getIngredientsForRecipeEdit(): Ingredient[] {
+    let ings: Ingredient[] = [];
+    this.ingredientService.getIngredients().subscribe(i => ings = i)!;
+    return ings;
+  }
+  getType(id: number) {
+    let string = "";
+    this.typeService.getType(id).subscribe(t => string = t.name);
+    return string;
+  }
+  addStepToRecipe() {
+    let newStep: Step = {
+      id: 0,
+      idRecipe: 0,
+      order: 0,
+      title: '',
+      body: ''
+    }
+    if (this.recipeToEdit.steps.length != 0 || !this.recipeToEdit.steps == undefined) {
+      const previousStep = this.recipeToEdit.steps[this.recipeToEdit.steps.length - 1];
+      newStep.id = previousStep.id + 1;
+      newStep.order = previousStep.order + 1;
+    }
+    newStep.idRecipe = this.recipeToEdit.id;
+    this.recipeToEdit.steps.push(newStep);
+  }
+  deleteStep(id: number) {
+    let index: number = this.recipeToEdit.steps.findIndex(s => s.id == id);
+    console.log("Deleted Step: " + JSON.stringify(this.recipeToEdit.steps[index]));
+    this.recipeToEdit.steps.splice(index, 1);
+  }
+  changeEdit() {
     this.edit = !this.edit;
+    if(this.edit){
+      this.recipeToEdit = Object.assign({}, this.recipe);
+    }
+    else{
+      this.recipeToEdit = {
+        id: 0,
+        name: '',
+        ingredients: [],
+        steps: [],
+        ingredientQuantity: []
+      };
+    }
   }
 }
