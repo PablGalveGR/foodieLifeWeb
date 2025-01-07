1aef22697937fc03a053fad5fa7258c21c4bd64d
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2025-01-07 22:19:32 +0100

    Analytics Implemeted

--- frontFoodieLife/src/app/app.component.ts
+++ frontFoodieLife/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -10,9 +12,21 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private router: Router) { }
+  firebaseConfig = {
+    apiKey: "AIzaSyBikdJPbVXmRCpak-vOnNQjFYABZOnImUo",
+    authDomain: "foddielife.firebaseapp.com",
+    projectId: "foddielife",
+    storageBucket: "foddielife.firebasestorage.app",
+    messagingSenderId: "10819365749",
+    appId: "1:10819365749:web:1f325834733988a92a013c",
+    measurementId: "G-VCDX5K4WNC"
+  };
+  app = initializeApp(this.firebaseConfig);
+  analytics = getAnalytics(this.app);
   title = 'FoodieLife';
-  constructor(private router : Router){}
-  goTo(url : string){
+  goTo(url: string) {
     this.router.navigate([url]);
   }
 }
